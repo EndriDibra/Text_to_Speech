65f819f6c8b23454863abf666d5e617cff195fa7
Endri Dibra <104068619+EndriDibra@users.noreply.github.com>
2022-05-21 01:18:19 +0300

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,17 @@
+# Using the required libraries
+from gtts import gTTS
+from playsound import playsound
+
+# Initializing the type of audio and language
+audio = 'speech.mp3'
+language = "en"
+
+# Taking input:(text) from the user
+text = input()
+
+# Initializing input, language
+sp = gTTS(text=text, lang=language, slow=False)
+
+# The input from the user will be saved and played by the computer using machine-voice
+sp.save(audio)
+playsound(audio)
